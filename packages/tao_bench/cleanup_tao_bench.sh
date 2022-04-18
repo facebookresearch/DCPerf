6b413badba34029b0319406c5d6027a3b1953a9f
Wei Su <wsu@fb.com>
2022-04-18 13:01:58 -0700

    Move tao_bench's cleanup script inside external/packages/tao_bench
    Summary: In the external version of Benchpress, move tao_bench's cleanup script inside packages/tao_bench/
    
    Reviewed By: meteorfox
    
    Differential Revision: D35722004
    
    fbshipit-source-id: 963bfe965e7c705b89450325ffaeb1cf77408226

--- packages/tao_bench/cleanup_tao_bench.sh
+++ packages/tao_bench/cleanup_tao_bench.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-BENCHPRESS_ROOT="$(dirname "$(readlink -f "$0")")"
+BPKGS_TAO_BENCH_ROOT="$(dirname "$(readlink -f "$0")")"
+BENCHPRESS_ROOT="$(readlink -f "$BPKGS_TAO_BENCH_ROOT/../..")"
 BENCHMARKS_DIR="${BENCHPRESS_ROOT}/benchmarks"
 TAO_BENCH_DIR="${BENCHMARKS_DIR}/tao_bench"
 
