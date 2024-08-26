be8ebfd08abeb33de0cf7eb70c523c6ad788ebdd
Wei Su <wsu@meta.com>
2024-08-26 10:33:19 -0700

    Add copyright header to new files
    Summary: Add copyright header to new files
    
    Reviewed By: ahmadelyoussef
    
    Differential Revision: D61800713
    
    fbshipit-source-id: ed37880c2cc963c18878076419890173962c12a1

--- benchpress/version.py
+++ benchpress/version.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 __PROJECT__ = "DCPerf"
 __VERSION__ = "0.2.0"
