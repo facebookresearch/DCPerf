3314690bfe1dc47924bc36e176debd0e8f9b0d60
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2025-01-29 11:07:57 -0800

    Add missing Pyre mode headers] [batch:8/1550] [shard:8/N]
    Differential Revision: D68820783
    
    fbshipit-source-id: fe951815ee25fa5565ae260c0248d734460a2983

--- benchpress/plugins/parsers/spark_standalone.py
+++ benchpress/plugins/parsers/spark_standalone.py
@@ -4,6 +4,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
+
 from benchpress.lib.baseline import BASELINES
 from benchpress.lib.parser import Parser
 
