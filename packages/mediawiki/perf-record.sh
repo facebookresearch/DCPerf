2a712e4179d83c03ebcf862dc873f46fdf9d9ece
Ahmad El Youssef <ahmadelyoussef@meta.com>
2025-03-12 18:06:54 -0700

    Adding copyrights headers
    Summary: Adding copyright headers
    
    Reviewed By: excelle08
    
    Differential Revision: D71081349
    
    fbshipit-source-id: e3d28b2220c6915df939a514c671711104856ad2

--- packages/mediawiki/perf-record.sh
+++ packages/mediawiki/perf-record.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+#
 
 sleep 30
 if [ -f "perf.data" ]; then
