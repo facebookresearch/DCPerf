87a9a074ffa52619d0d52092ea3240a8da00241e
Wei Su <wsu@meta.com>
2025-11-05 10:08:28 -0800

    DjangoBench V2: Enable installation of proxygen_binding (#286)
    Summary:
    
    Enable the `install` command in setup.py of python_binding module
    
    Reviewed By: marziehlenjaniMeta
    
    Differential Revision: D84732070

--- packages/django_workload/srcs/proxygen_binding/setup.py
+++ packages/django_workload/srcs/proxygen_binding/setup.py
@@ -227,9 +227,36 @@ setup(
     version="0.1.0",
     author="Meta Platforms, Inc.",
     description="Python bindings for Proxygen HTTP server",
-    long_description="",
+    long_description=open("README.md").read() if os.path.exists("README.md") else "",
+    long_description_content_type="text/markdown",
     ext_modules=ext_modules,
+    py_modules=[
+        "django_asgi_adapter",
+        "example_server",
+        "django_server",
+    ],
     cmdclass={"build_ext": build_ext},
     zip_safe=False,
     python_requires=">=3.8",
+    install_requires=[
+        "pybind11>=2.6.0",
+    ],
+    extras_require={
+        "django": ["django>=3.0"],
+    },
+    scripts=[
+        "example_server.py",
+        "django_server.py",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: C++",
+    ],
 )
