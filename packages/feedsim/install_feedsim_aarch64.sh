a10e296eab0cdf83f91bc53b8d3c202d3b3deb56
Lukas Alt <lalt@nvidia.com>
2025-11-25 16:18:21 +0100

    Fixes and cleanup for feedsim install script

--- packages/feedsim/install_feedsim_aarch64.sh
+++ packages/feedsim/install_feedsim_aarch64.sh
@@ -5,7 +5,6 @@
 # LICENSE file in the root directory of this source tree.
 
 set -Eeuo pipefail
-# trap cleanup SIGINT SIGTERM ERR EXIT
 
 # Constants
 FEEDSIM_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
@@ -13,10 +12,6 @@ BENCHPRESS_ROOT="$(readlink -f "$FEEDSIM_ROOT/../..")"
 FEEDSIM_ROOT_SRC="${BENCHPRESS_ROOT}/benchmarks/feedsim"
 FEEDSIM_THIRD_PARTY_SRC="${FEEDSIM_ROOT_SRC}/third_party"
 echo "BENCHPRESS_ROOT is ${BENCHPRESS_ROOT}"
-
-cleanup() {
-  trap - SIGINT SIGTERM ERR EXIT
-}
 
 msg() {
   echo >&2 -e "${1-}"
@@ -27,6 +22,24 @@ die() {
   local code=${2-1} # default exit status 1
   msg "$msg"
   exit "$code"
+}
+
+verify_checksum() {
+    local file="$1"
+    local expected_checksum="$2"
+
+    if ! [ -f "$file" ]; then
+        echo "File not found: $file"
+        exit 1
+    fi
+
+    local actual_checksum
+    actual_checksum=$(sha256sum "$file" | awk '{print $1}')
+
+    if [[ "$actual_checksum" != "$expected_checksum" ]]; then
+        echo "WARNING: Checksum mismatch for file: $file"
+        exit 1
+    fi
 }
 
 dnf install -y bc ninja-build flex bison git texinfo binutils-devel \
@@ -57,6 +70,7 @@ cd "${FEEDSIM_THIRD_PARTY_SRC}"
 # Installing cmake-4.0.3
 if ! [ -d "cmake-4.0.3" ]; then
     wget "https://github.com/Kitware/CMake/releases/download/v4.0.3/cmake-4.0.3.tar.gz"
+    verify_checksum "cmake-4.0.3.tar.gz" "8d3537b7b7732660ea247398f166be892fe6131d63cc291944b45b91279f3ffb"
     tar -zxf "cmake-4.0.3.tar.gz"
     cd "cmake-4.0.3"
     mkdir staging
@@ -70,14 +84,18 @@ fi
 
 export PATH="${FEEDSIM_THIRD_PARTY_SRC}/cmake-4.0.3/staging/bin:${PATH}"
 
+# Installing fast_float
 if ! [ -d "fast_float" ]; then
     git clone https://github.com/fastfloat/fast_float.git
     cd fast_float
+    git checkout v8.1.0
     mkdir build && cd build
     cmake ..
-    make
+    make -j"$(nproc)"
     make install
-    cd ../
+    cd ../../
+else
+    msg "[SKIPPED] fast_float"
 fi
 
 # Installing gengetopt
@@ -85,6 +103,7 @@ if ! [ -d "gengetopt-2.23" ]; then
     # Source the download retry function
     source "${BENCHPRESS_ROOT}/scripts/download_with_retry.sh"
     download_with_retry "https://mirrors.ocf.berkeley.edu/gnu/gengetopt/gengetopt-2.23.tar.xz"
+    verify_checksum "gengetopt-2.23.tar.xz" "b941aec9011864978dd7fdeb052b1943535824169d2aa2b0e7eae9ab807584ac"
     tar -xf "gengetopt-2.23.tar.xz"
     cd "gengetopt-2.23"
     ./configure
@@ -98,24 +117,25 @@ fi
 # Installing Boost
 if ! [ -d "boost_1_88_0" ]; then
     wget "https://archives.boost.io/release/1.88.0/source/boost_1_88_0.tar.gz"
+    verify_checksum "boost_1_88_0.tar.gz" "3621533e820dcab1e8012afd583c0c73cf0f77694952b81352bf38c1488f9cb4"
     tar -xzf "boost_1_88_0.tar.gz"
     cd "boost_1_88_0"
     ./bootstrap.sh --without-libraries=python
-    ./b2 install
+    ./b2 -j"$(nproc)" install
     cd ../
 else
     msg "[SKIPPED] boost_1_88_0"
 fi
 
-
 # Installing gflags
 if ! [ -d "gflags-2.2.2" ]; then
     wget "https://github.com/gflags/gflags/archive/refs/tags/v2.2.2.tar.gz" -O "gflags-2.2.2.tar.gz"
+    verify_checksum "gflags-2.2.2.tar.gz" "34af2f15cf7367513b352bdcd2493ab14ce43692d2dcd9dfc499492966c64dcf"
     tar -xzf "gflags-2.2.2.tar.gz"
     cd "gflags-2.2.2"
     mkdir -p build && cd build
     cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ../
-    make -j8
+    make -j"$(nproc)"
     make install
     cd ../../
 else
@@ -125,11 +145,12 @@ fi
 # Installing glog
 if ! [ -d "glog-0.4.0" ]; then
     wget "https://github.com/google/glog/archive/refs/tags/v0.4.0.tar.gz" -O "glog-0.4.0.tar.gz"
+    verify_checksum "glog-0.4.0.tar.gz" "f28359aeba12f30d73d9e4711ef356dc842886968112162bc73002645139c39c"
     tar -xzf "glog-0.4.0.tar.gz"
     cd "glog-0.4.0"
     mkdir -p build && cd build
     cmake -DBUILD_SHARED_LIBS=ON -DBUILD_TESTING=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ../
-    make -j8
+    make -j"$(nproc)"
     make install
     cd ../../
 else
@@ -139,6 +160,7 @@ fi
 # Installing JEMalloc
 if ! [ -d "jemalloc-5.3.0" ]; then
     wget "https://github.com/jemalloc/jemalloc/releases/download/5.3.0/jemalloc-5.3.0.tar.bz2"
+    verify_checksum "jemalloc-5.3.0.tar.bz2" "2db82d1e7119df3e71b7640219b6dfe84789bc0537983c3b7ac4f7189aecfeaa"
     bunzip2 "jemalloc-5.3.0.tar.bz2"
     tar -xvf "jemalloc-5.3.0.tar"
     cd "jemalloc-5.3.0"
@@ -153,6 +175,7 @@ fi
 # Installing libevent
 if ! [ -d "libevent-2.1.12-stable" ]; then
     wget "https://github.com/libevent/libevent/releases/download/release-2.1.12-stable/libevent-2.1.12-stable.tar.gz"
+    verify_checksum "libevent-2.1.12-stable.tar.gz" "92e6de1be9ec176428fd2367677e61ceffc2ee1cb119035037a27d346b0403bb"
     tar -xzf "libevent-2.1.12-stable.tar.gz"
     cd "libevent-2.1.12-stable"
     ./configure
@@ -164,13 +187,10 @@ else
 fi
 
 msg "Installing third-party dependencies ... DONE"
-
-
 # Installing FeedSim
-cd "${FEEDSIM_ROOT_SRC}"
+cd "${FEEDSIM_ROOT_SRC}/src"
 
-cd "src"
-
+msg "Initializing third-party submodules"
 # Populate third party submodules
 while read -r submod;
 do
@@ -188,6 +208,7 @@ do
     fi
 
 done < "${FEEDSIM_ROOT}/submodules.txt"
+msg "Initializing third-party submodules ... DONE"
 
 # Patch fizz for OpenSSL 3.0 compatibility
 if [ -f "third_party/fizz/fizz/tool/FizzServerCommand.cpp" ]; then
@@ -195,26 +216,25 @@ if [ -f "third_party/fizz/fizz/tool/FizzServerCommand.cpp" ]; then
     sed -i 's/EVP_PKEY_cmp(pubKey.get(), key.get()) == 1/EVP_PKEY_eq(pubKey.get(), key.get())/g' "third_party/fizz/fizz/tool/FizzServerCommand.cpp"
 fi
 
+msg "Building FeedSim ..."
 mkdir -p build && cd build/
 
 # Build FeedSim
+FS_CC="${BP_CC:-gcc}"
+FS_CXX="${BP_CC:-g++}"
 FS_CFLAGS="${BP_CFLAGS:--O3 -DNDEBUG}"
 FS_CXXFLAGS="${BP_CXXFLAGS:--O3 -DNDEBUG -Wno-deprecated-declarations}"
 FS_LDFLAGS="${BP_LDFLAGS:-} -latomic -Wl,--export-dynamic"
 
-BP_CC=gcc
-BP_CXX=g++
-
-# Use system OpenSSL
-
 cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH="${FEEDSIM_THIRD_PARTY_SRC}/build-deps" \
-    -DCMAKE_C_COMPILER="$BP_CC" \
-    -DCMAKE_CXX_COMPILER="$BP_CXX" \
+    -DCMAKE_C_COMPILER="$FS_CC" \
+    -DCMAKE_CXX_COMPILER="$FS_CXX" \
     -DCMAKE_C_FLAGS_RELEASE="$FS_CFLAGS" \
     -DCMAKE_CXX_FLAGS_RELEASE="$FS_CXXFLAGS -DFMT_HEADER_ONLY=1" \
     -DCMAKE_EXE_LINKER_FLAGS_RELEASE="$FS_LDFLAGS" \
     ../
 
 ninja-build -v -j1
+msg "Building FeedSim ... DONE"
