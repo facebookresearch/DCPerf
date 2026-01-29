09c195f14082ac227c3e20ea7ec1135c8874b972
Samir Farhat <sfarhatd@meta.com>
2026-01-29 08:02:45 -0800

    Configuration for fio based flash micro benchmark (#417)
    Summary:
    
    This constitutes the initial configuration for a fio based flash microbenchmark. Implementation to follow(some args may be culled later if during implementation I don't find a use for it).
    
    Differential Revision: D91732325

---
+++ packages/cdn_bench/micro_flash/install_flash_micro.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+set -Eeuo pipefail
+echo "Installing Flash Micro"
+
+LINUX_DIST_ID="$(awk -F "=" '/^ID=/ {print $2}' /etc/os-release | tr -d '"')"
+
+##########################################
+# Install prerequisite packages
+##########################################
+echo "Installing prerequisite packages..."
+if [ "$LINUX_DIST_ID" = "ubuntu" ]; then
+  apt install -y fio
+elif [ "$LINUX_DIST_ID" = "centos" ]; then
+  dnf install -y fio
+fi
+
+echo "Installation complete!"
