68adad3dda89a39b1859cd0b6e3ef6ce74cca42f
Ahmad El Youssef <ahmadelyoussef@meta.com>
2026-02-04 10:43:36 -0800

    Running CI for wdl
    Differential Revision: D92300799

--- packages/wdl_bench/install_wdl_bench.sh
+++ packages/wdl_bench/install_wdl_bench.sh
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 set -Eeuo pipefail
 
+
 GLIBC_VERSION=$(getconf GNU_LIBC_VERSION | cut -f 2 -d\  )
 
 ##################### BENCHMARK CONFIG #########################
