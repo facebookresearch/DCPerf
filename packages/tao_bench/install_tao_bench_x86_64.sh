9114dadd68c36524e43c716c9d042b45b9cf9bf7
Ahmad El Youssef <ahmadelyoussef@meta.com>
2026-08-18 13:00:55 -0700

    fix liburing version in ubuntu 22.04 since it is not enough to build folly
    Reviewed By: excelle08
    
    Differential Revision: D115634471

--- packages/tao_bench/install_tao_bench_x86_64.sh
+++ packages/tao_bench/install_tao_bench_x86_64.sh
@@ -75,6 +75,23 @@ if ! [ -f "/usr/local/bin/cmake" ]; then
     ln -s /usr/bin/cmake /usr/local/bin/cmake
 fi
 
+# Folly requires APIs newer than Ubuntu 22.04's liburing 2.1; pin 2.12 for reproducible builds.
+LIBURING_VERSION="2.12"
+if [ "$LINUX_DIST_ID" = "ubuntu" ] && ! PKG_CONFIG_LIBDIR="${TAO_BENCH_DEPS}/lib/pkgconfig" \
+    pkg-config --atleast-version="${LIBURING_VERSION}" liburing; then
+    LIBURING_ROOT="${TAO_BENCH_ROOT}/liburing-${LIBURING_VERSION}"
+    if ! [ -d "${LIBURING_ROOT}" ]; then
+        git clone --branch "liburing-${LIBURING_VERSION}" --depth 1 \
+            https://github.com/axboe/liburing.git "${LIBURING_ROOT}"
+    fi
+    pushd "${LIBURING_ROOT}"
+    ./configure --prefix="${TAO_BENCH_DEPS}"
+    make -j"${NUM_BUILD_JOBS}"
+    make liburing.pc
+    make install
+    popd
+fi
+
 # Install openssl
 if ! [ -d "openssl" ]; then
     git clone --branch "${OPENSSL_BRANCH}" --depth 1 https://github.com/openssl/openssl.git
@@ -121,6 +138,8 @@ pushd folly
 git checkout v2024.06.24.00
 git apply "${BENCHPRESS_ROOT}/packages/tao_bench/folly_zlib_uri.patch"
 sed -i 's/FOLLY_ALWAYS_INLINE//g' "${TAO_BENCH_ROOT}/folly/folly/experimental/symbolizer/StackTrace.cpp"
+CMAKE_PREFIX_PATH="${TAO_BENCH_DEPS}" \
+PKG_CONFIG_PATH="${TAO_BENCH_DEPS}/lib/pkgconfig" \
 OPENSSL_ROOT_DIR="${TAO_BENCH_DEPS}" ./build/fbcode_builder/getdeps.py --allow-system-packages build \
     --scratch-path "${FOLLY_BUILD_ROOT}"
 popd
